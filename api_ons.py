1b16ed02d83eba27f5ad6a9134ddf4d5ae6f69b3
Pedro Luís Azevedo Costa <pdcost@icloud.com>
2025-02-01 23:11:45 +0000

    Using delete_matching to enable reprocessing

--- api_ons.py
+++ api_ons.py
@@ -61,22 +61,47 @@ class ApiWrapper:
         else:
             print("Bucket already exists")
 
+    #def get_partition
+
+    def read_data_parquet(self, cod_areacarga, year, month, day):
+        """
+        Reads a parquet file from a S3 bucket
+        """
+        s3_path = f"s3://{self.s3_bucket_name}/test-partition/data.parquet/cod_areacarga={cod_areacarga}/val_year={year}/val_month={month}/val_day={day}/"
+
+        df = pd.read_parquet(s3_path, engine="pyarrow")
+
+        return df
+
     def save_data_parquet(self, df):
         """
         Saves a dataframe as parquet file in a S3 bucket
         """
-        s3_path = f"s3://{self.s3_bucket_name}/data.parquet"
+        s3_path = f"s3://{self.s3_bucket_name}/test-partition/data.parquet"
 
         # Criar um sistema de arquivos S3
         fs = s3fs.S3FileSystem()
+
+        # Converts dt_referencia to datetime
+        df["dat_referencia"] = pd.to_datetime(df["dat_referencia"])
+
+        # Partiton into year, month, day
+        df["val_year"] = df["dat_referencia"].dt.year
+        df["val_month"] = df["dat_referencia"].dt.month
+        df["val_day"] = df["dat_referencia"].dt.day
 
         df.to_parquet(
             s3_path,
             engine="pyarrow",
             filesystem=fs,  # Passa o sistema de arquivos S3FS
-            partition_cols=["dat_referencia"],
+            partition_cols=["cod_areacarga", "val_year", "val_month", "val_day"], # Needs to change to year, month, day,
+            existing_data_behavior="delete_matching",
+            compression="snappy",
         )
-        pass
+
+        return
+
+    
 
 
 
@@ -85,7 +110,7 @@ if __name__ == "__main__":
     api = ApiOns()
 
     # print("Teste de carga verificada")
-    resp_verificada = api.get_carga_verificada("2021-01-01", "2021-01-02", "SP")
+    resp_verificada = api.get_carga_verificada("2021-01-03", "2021-01-04", "SP")
     # print(resp_verificada.status_code)
     # if resp_verificada.status_code == 200:
     #     print(resp_verificada.content)
@@ -101,5 +126,3 @@ if __name__ == "__main__":
     api_wrapper.create_s3_bucket()
 
     df = pd.DataFrame(resp_verificada)
-
-    api_wrapper.save_data_parquet(df)
